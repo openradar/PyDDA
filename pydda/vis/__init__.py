f558a898170c57177feca1cf3aa4e5be502ed31c
Robert Jackson <rjackson@anl.gov>
2018-06-13 15:23:40 -0500

    ADD: Streamline plots to visualization module

--- pydda/vis/__init__.py
+++ pydda/vis/__init__.py
@@ -13,11 +13,17 @@ A visualization module for plotting generated wind fields.
     plot_horiz_xsection_barbs
     plot_xz_xsection_barbs
     plot_yz_xsection_barbs
+    plot_horiz_xsection_streamlines
+    plot_xz_xsection_streamlines
+    plot_yz_xsection_streamlines
 
 """
 
 
 from .barb_plot import plot_horiz_xsection_barbs, plot_xz_xsection_barbs
 from .barb_plot import plot_yz_xsection_barbs
+from .streamline_plot import plot_horiz_xsection_streamlines
+from .streamline_plot import plot_xz_xsection_streamlines
+from .streamline_plot import plot_yz_xsection_streamlines
 
 
