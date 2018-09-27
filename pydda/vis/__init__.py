8237a5b2e06e0d376d56f9d4ab3be2c440ae7062
Robert Jackson <rjackson@anl.gov>
2018-09-27 12:53:38 -0500

    ADD: We can now plot barb plots on a geographic map. Reflectivity fields are now combined in display.

--- pydda/vis/__init__.py
+++ pydda/vis/__init__.py
@@ -21,7 +21,7 @@ A visualization module for plotting generated wind fields.
 
 
 from .barb_plot import plot_horiz_xsection_barbs, plot_xz_xsection_barbs
-from .barb_plot import plot_yz_xsection_barbs
+from .barb_plot import plot_yz_xsection_barbs, plot_horiz_xsection_barbs_map
 from .streamline_plot import plot_horiz_xsection_streamlines
 from .streamline_plot import plot_xz_xsection_streamlines
 from .streamline_plot import plot_yz_xsection_streamlines
