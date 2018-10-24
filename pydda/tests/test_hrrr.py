69134ce85a67609546dabe2c18a9e0125577dd9e
Robert Jackson <rjackson@anl.gov>
2018-10-24 16:19:07 -0500

    MAINT: Skip HRRR test on Travis, just make sure you can add the data.

--- pydda/tests/test_hrrr.py
+++ pydda/tests/test_hrrr.py
@@ -12,7 +12,9 @@ except:
     CFGRIB_AVAILABLE = 0
 
 
+# Test succeeds on workstation, but crashes Travis CI with killed error (too much memory?)
 @pytest.mark.skipif(CFGRIB_AVAILABLE == 0, reason="Cfgrib not installed!")
+@pytest.mark.skipIf("TRAVIS" in os.environ and os.environ["TRAVIS"] == "true", reason="Skipping this test on Travis CI.")
 def test_hrrr_constraint():
     hrrr_url = 'https://pando-rgw01.chpc.utah.edu/hrrr/prs/20180914/hrrr.t06z.wrfprsf00.grib2'
     r = requests.get(hrrr_url)
@@ -38,5 +40,24 @@ def test_hrrr_constraint():
     assert u.min() < 40
     assert v.max() > 40
     assert v.min() < 40
+
+
+def test_add_hrrr_data():
+    hrrr_url = 'https://pando-rgw01.chpc.utah.edu/hrrr/prs/20180914/hrrr.t06z.wrfprsf00.grib2'
+    r = requests.get(hrrr_url)
+    temp_file = tempfile.NamedTemporaryFile(delete=False)
+    with temp_file as f:
+        f.write(r.content)
+    grid_mhx = pyart.io.read_grid(pydda.tests.MHX_GRID)
+    grid_mhx = pydda.constraints.add_hrrr_constraint_to_grid(grid_mhx,
+               temp_file.name)
+ 
+    u = grid_mhx.fields["U_hrrr"]["data"]
+    v = grid_mhx.fields["V_hrrr"]["data"]
+    # Are we actually retrieving a hurricane?
+    assert u.max() > 40
+    assert u.min() < 40
+    assert v.max() > 40
+    assert v.min() < 40 
 
     
