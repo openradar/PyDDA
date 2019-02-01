c79626c8c77e5454cd0f9eb41ecdc2d84913b559
Robert Jackson <rjackson@anl.gov>
2019-02-01 12:58:25 -0600

    STY: Pep8

--- pydda/retrieval/nesting.py
+++ pydda/retrieval/nesting.py
@@ -10,6 +10,8 @@ from copy import deepcopy
 from datetime import datetime
 from .wind_retrieve import get_dd_wind_field
 
+
+# Reduces the resolution of a PyART grid
 def _reduce_pyart_grid_res(Grid, skip_factor):
     Grid2 = deepcopy(Grid)
     field_dict = {}
@@ -17,7 +19,7 @@ def _reduce_pyart_grid_res(Grid, skip_factor):
         field_dict[field_name] = Grid2.fields[field_name].copy()
         field_dict[field_name]["data"] = Grid2.fields[field_name]["data"][
             :, ::skip_factor, ::skip_factor]
-        
+
     x = Grid2.x
     x["data"] = x["data"][::skip_factor]
     y = Grid2.y
@@ -38,18 +40,19 @@ def _reduce_pyart_grid_res(Grid, skip_factor):
     new_grid = pyart.core.Grid(
         gtime, field_dict, metadata, origin_latitude, origin_longitude,
         origin_altitude, x, y, z, projection, radar_latitude, radar_longitude,
-        radar_altitude, radar_time, radar_name) 
+        radar_altitude, radar_time, radar_name)
     del Grid2
     return new_grid
 
 
+# Splits a Py-ART Grid
 def _split_pyart_grid(Grid, split_factor, axis=1):
     grid_splits = []
     split_field = {}
     Grid2 = deepcopy(Grid)
     for field_name in Grid2.fields.keys():
         if isinstance(Grid2.fields[field_name]["data"], np.ma.MaskedArray):
-            no_mask = Grid2.fields[field_name]["data"].filled(np.nan).copy() 
+            no_mask = Grid2.fields[field_name]["data"].filled(np.nan).copy()
         else:
             no_mask = Grid2.fields[field_name]["data"].copy()
         split_field[field_name] = np.array_split(
@@ -89,7 +92,7 @@ def _split_pyart_grid(Grid, split_factor, axis=1):
             x_dic["data"] = x_split[i]
         elif(axis == 0):
             z_dic["data"] = z_split[i]
-        
+
         new_grid = pyart.core.Grid(
             gtime, grid_dic, metadata, origin_latitude, origin_longitude,
             origin_altitude, x_dic, y_dic, z_dic, projection, radar_latitude,
@@ -99,6 +102,7 @@ def _split_pyart_grid(Grid, split_factor, axis=1):
     return grid_splits
 
 
+# Concatenates Py-ART Grids
 def _concatenate_pyart_grids(grid_list, axis=1):
     new_grid = deepcopy(grid_list[0])
     for field_name in new_grid.fields.keys():
@@ -114,9 +118,10 @@ def _concatenate_pyart_grids(grid_list, axis=1):
         new_grid.ny = np.sum([x.ny for x in grid_list])
     elif(axis == 0):
         new_grid.z["data"] = np.ma.concatenate(
-            [x.z["data"] for x in grid_list]) 
+            [x.z["data"] for x in grid_list])
         new_grid.nz = np.sum([x.nz for x in grid_list])
     return new_grid
+
 
 # Procedure: 1. Do first pass of retrieval on reduced resolution grid
 # 2. Then, we use the reduced resolution retrieval as an input to the
@@ -126,22 +131,22 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
                              reduction_factor=2, num_splits=2, **kwargs):
     """
     This function performs a wind retrieval using a nested domain.
-    This is useful for grids that are larger than about 500 by 500 
+    This is useful for grids that are larger than about 500 by 500
     by 40 points, since the use of larger grids on a single machine
-    will exceed memory limitations. 
-    
+    will exceed memory limitations.
+
     This procedure relies on a dask distributed cluster to be set up.
     The retrieval is first performed at a resolution that is coarser
     than the analysis grid by reduction_factor. This provides the
-    initial state for the nested loop. 
-    
+    initial state for the nested loop.
+
     The domain is split into num_splits**2 sub-domains for the nested
     retrieval step, and each nested retrieval is mapped onto a distributed
-    worker for parallel processing. If NumPy and SciPy are already set up to 
+    worker for parallel processing. If NumPy and SciPy are already set up to
     use parallel numerical analysis libraries, it is recommended that a single
     machine be dedicated to each nest rather than a single core
     for best peformance.
-    
+
     Parameters
     ==========
     grid_list: list
@@ -150,33 +155,33 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
        The initial guess of the zonal wind field. This has to be in the same
        shape as the analysis grid.
     v_init: 3D NumPy array
-       The initial guess of the meridional wind field. This has to be in the 
+       The initial guess of the meridional wind field. This has to be in the
        same shape as the analysis grid.
     w_init: 3D NumPy array
        The initial guess of the vertical wind field. This has to be in the same
        shape as the analysis grid.
     client: dask distributed Client
        The distributed Client that is linked to a distributed cluster. The
-       cluster must be running before get_dd_wind_field_nested is called. 
+       :cluster must be running before get_dd_wind_field_nested is called.
        The retrieval on each nest will be mapped onto each worker. Since
        the optimization loop already takes advantage of parallelism, it's
        best to allow at least 16 cores per one worker.
     reduction_factor: int
        How much to reduce the factor of the analysis grid by when doing the
        initial retrieval on the entire grid.
-    
+
     **kwargs: dict
-        This function will take the same keyword arguments as 
-        get_dd_wind_field, as these arguments are passed into each call of 
-        get_dd_wind_field. See get_dd_wind_field for more information on the 
+        This function will take the same keyword arguments as
+        get_dd_wind_field, as these arguments are passed into each call of
+        get_dd_wind_field. See get_dd_wind_field for more information on the
     """
     # First, we do retrieval on whole grid with fraction of resolution
     grid_lo_res_list = [_reduce_pyart_grid_res(G, reduction_factor)
                         for G in grid_list]
 
     first_pass = get_dd_wind_field(
-        grid_lo_res_list, u_init[::, ::reduction_factor, ::reduction_factor], 
-        v_init[::, ::reduction_factor, ::reduction_factor], 
+        grid_lo_res_list, u_init[::, ::reduction_factor, ::reduction_factor],
+        v_init[::, ::reduction_factor, ::reduction_factor],
         w_init[::, ::reduction_factor, ::reduction_factor], **kwargs)
 
     # Take the first pass field and regrid to analysis field
@@ -187,14 +192,14 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
     y = grid_list[0].point_y["data"].flatten()
     z = grid_list[0].point_z["data"].flatten()
     u_init_new = griddata((reduced_z, reduced_y, reduced_x),
-                           first_pass[0].fields["u"]["data"].flatten(), 
-                           (z, y, x), method='nearest')
+                          first_pass[0].fields["u"]["data"].flatten(),
+                          (z, y, x), method='nearest')
     v_init_new = griddata((reduced_z, reduced_y, reduced_x),
-                           first_pass[0].fields["v"]["data"].flatten(),
-                           (z, y, x), method='nearest')
+                          first_pass[0].fields["v"]["data"].flatten(),
+                          (z, y, x), method='nearest')
     w_init_new = griddata((reduced_z, reduced_y, reduced_x),
-                           first_pass[0].fields["w"]["data"].flatten(),
-                           (z, y, x), method='nearest')
+                          first_pass[0].fields["w"]["data"].flatten(),
+                          (z, y, x), method='nearest')
     u_init_new = np.reshape(u_init_new, u_init.shape)
     v_init_new = np.reshape(v_init_new, v_init.shape)
     w_init_new = np.reshape(w_init_new, w_init.shape)
@@ -213,19 +218,19 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
             grid_fns = []
             j = 0
             for g in g_list:
-                fn = (tempfile_name_base + str(k) + '.' + 
+                fn = (tempfile_name_base + str(k) + '.' +
                       str(i) + '.' + str(j) + '.nc')
-                pyart.io.write_grid((tempfile_name_base + str(k) + 
+                pyart.io.write_grid((tempfile_name_base + str(k) +
                                      '.' + str(i) + '.' + str(j) + '.nc'), g)
                 j = j + 1
                 grid_fns.append(fn)
             cur_list.append(grid_fns)
             i = i + 1
         del split_grids_x, g_list
-        
+
         k = k + 1
         tiny_grids.append(cur_list)
-    
+
     # Temporarily save the tiny grids and free up memory...we want to
     # load these when we are running it on the cluster
 
@@ -238,20 +243,21 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
     v_init_split_x = np.array_split(v_init_new, num_splits, axis=2)
     v_init_split = [np.array_split(vx, num_splits, axis=1)
                     for vx in v_init_split_x]
-    
+
     # Clear out unneeded variables (do not need lo-res grids in memory anymore)
     del u_init_split_x, w_init_split_x, v_init_split_x
     del first_pass, reduced_x, reduced_y, reduced_z, x, y, z, grid_lo_res_list
     gc.collect()
-    
+
     # Serial just for testing, need to use dask in future
     tiny_retrieval = []
-    def do_tiny_retrieval(i,j):
+
+    def do_tiny_retrieval(i, j):
         tgrids = [pyart.io.read_grid(tiny_grids[k][i][j])
                   for k in range(len(grid_list))]
-        new_grids = get_dd_wind_field(tgrids, 
-            u_init_split[i][j], v_init_split[i][j], w_init_split[i][j],
-            **kwargs)
+        new_grids = get_dd_wind_field(
+            tgrids, u_init_split[i][j], v_init_split[i][j],
+            w_init_split[i][j], **kwargs)
         del tgrids
         gc.collect()
         return new_grids
@@ -262,11 +268,11 @@ def get_dd_wind_field_nested(grid_list, u_init, v_init, w_init, client,
             futures_array.append(client.submit(do_tiny_retrieval, i, j))
 
     print("Waiting for nested grid to be retrieved...")
-    wait(futures_array)  
+    wait(futures_array)
     tiny_retrieval2 = client.gather(futures_array)
     tiny_retrieval = []
 
-    for i in range(num_splits):    
+    for i in range(num_splits):
         new_grid_list = []
 
         for j in range(len(grid_list)):
