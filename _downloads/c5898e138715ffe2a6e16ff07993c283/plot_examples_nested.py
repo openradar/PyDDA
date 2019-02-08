9ba3636def1d822be7c822bf8e9b0674867db095
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-02-08 21:10:21 +0000

    Update docs after building Travis build 113 of openradar/PyDDA
    The docs were built from the branch 'master' against the commit
    c6ad8cd8d23bcacf5453c7e71b08756aff207654.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/openradar/PyDDA/jobs/490729045.
    
    The doctr command that was run is
    
        /home/travis/miniconda3/envs/testenv/bin/doctr deploy .

--- _downloads/c5898e138715ffe2a6e16ff07993c283/plot_examples_nested.py
+++ _downloads/c5898e138715ffe2a6e16ff07993c283/plot_examples_nested.py
@@ -25,7 +25,7 @@ if __name__ == '__main__':
 
     # Load sounding data and insert as an intialization
     u_init, v_init, w_init = pydda.initialization.make_wind_field_from_profile(
-        cpol_grid, sounding[1], vel_field='VT')
+        cpol_grid, sounding[1], vel_field='corrected_velocity')
 
     # Start our dask distributed cluster. You can use any distributed cluster
     # for this...a LocalCluster is used here for the sake of being able to run
@@ -39,12 +39,13 @@ if __name__ == '__main__':
     # and data weighting constraints.
     Grids = pydda.retrieval.get_dd_wind_field_nested(
         [berr_grid, cpol_grid], u_init,  v_init, w_init, client, Co=10.0,
-        Cm=1500.0, Cz=0, vel_name='VT', refl_field='DT', frz=5000.0,
+        Cm=1500.0, Cz=0, frz=5000.0,
         filt_iterations=2, mask_outside_opt=True, upper_bc=1)
 
     # Plot a horizontal cross section
     plt.figure(figsize=(9, 9))
-    pydda.vis.plot_horiz_xsection_barbs(Grids, background_field='DT', level=6,
+    pydda.vis.plot_horiz_xsection_barbs(Grids, background_field='reflectivity', 
+                                        level=6,
                                         w_vel_contours=[3, 6, 9, 12, 15],
                                         barb_spacing_x_km=5.0,
                                         barb_spacing_y_km=15.0)
@@ -52,7 +53,8 @@ if __name__ == '__main__':
 
     # Plot a vertical X-Z cross section
     plt.figure(figsize=(9, 9))
-    pydda.vis.plot_xz_xsection_barbs(Grids, background_field='DT', level=40,
+    pydda.vis.plot_xz_xsection_barbs(Grids, background_field='reflectivity', 
+                                     level=40,
                                      w_vel_contours=[3, 6, 9, 12, 15],
                                      barb_spacing_x_km=10.0,
                                      barb_spacing_z_km=2.0)
@@ -60,7 +62,8 @@ if __name__ == '__main__':
 
     # Plot a vertical Y-Z cross section
     plt.figure(figsize=(9, 9))
-    pydda.vis.plot_yz_xsection_barbs(Grids, background_field='DT', level=40,
+    pydda.vis.plot_yz_xsection_barbs(Grids, background_field='reflectivity',
+                                     level=40,
                                      w_vel_contours=[3, 6, 9, 12, 15],
                                      barb_spacing_y_km=10.0,
                                      barb_spacing_z_km=2.0)
