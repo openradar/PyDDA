a3d2be97384b91dcd1d465e7c5b96c5d36411ac7
Bobby Jackson <rjackson@anl.gov>
2024-01-19 14:05:41 -0600

    ADD: io submodule

---
+++ pydda/io/__init__.py
@@ -0,0 +1,17 @@
+"""
+=================================
+pydda.io (pydda.io)
+=================================
+
+.. currentmodule:: pydda.io
+
+This module handles the input/ouput of PyDDA.
+
+.. autosummary::
+    :toctree: generated/
+
+    read_grid
+    read_from_pyart_grid
+"""
+
+from .read_grid import read_grid, read_from_pyart_grid
